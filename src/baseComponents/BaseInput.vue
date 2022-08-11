fd703320b79a8662ce653e227f88024b3fe2d64a
Jerzy <jurekledzinski@yahoo.pl>
2022-08-11 08:49:12 +0200

    Hide arrows in input number for firefox ...

--- src/baseComponents/BaseInput.vue
+++ src/baseComponents/BaseInput.vue
@@ -31,10 +31,14 @@ export default {
   outline: none;
 }
 
-input::-webkit-outer-spin-button,
-input::-webkit-inner-spin-button {
+input[type='number']::-webkit-outer-spin-button,
+input[type='number']::-webkit-inner-spin-button {
   -webkit-appearance: none;
   margin: 0;
+}
+
+input[type='number'] {
+  -moz-appearance: textfield;
 }
 
 .input--error {
